c59c5fa04dc268ff85df5be3b4bd436b123ecdc1
gretmn102 <erlgreywatson@gmail.com>
2023-05-09 19:53:13 +0300

    solve 10 task

---
+++ Task10/src/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите кол-во элементов для генерации: ");
+        int count = Int32.Parse(Console.ReadLine());
+
+        int initNumber = 5;
+        int diff = 7;
+
+        for (int i = 0; i < count; i++)
+        {
+            int currentNumber = (i * diff) + initNumber;
+            Console.Write($"{currentNumber} ");
+        }
+        Console.WriteLine();
+    }
+}
