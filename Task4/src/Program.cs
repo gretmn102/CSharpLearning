007abff971647ca6a9dec3d9731e290eb80bbdb8
gretmn102 <erlgreywatson@gmail.com>
2023-05-09 17:38:55 +0300

    solve the 4 task

---
+++ Task4/src/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static string PrintAndReadLine(string description, string defaultValue)
+    {
+        System.Console.Write(description);
+        string? answer = System.Console.ReadLine();
+        if (String.IsNullOrEmpty(answer))
+        {
+            return defaultValue;
+        }
+        return answer;
+    }
+
+    private static void Main(string[] args)
+    {
+        int imagesCount = 52;
+        int rowImagesCount = 3;
+
+        int fullRowsCount = imagesCount / rowImagesCount;
+
+        int imagesLeftCount = imagesCount % rowImagesCount;
+
+        Console.WriteLine($"Полностью заполненных рядов — {fullRowsCount}");
+        Console.WriteLine($"Не вместилось изображений — {imagesLeftCount}");
+    }
+}
