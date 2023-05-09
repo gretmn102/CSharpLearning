9d4be407f3b8c692e3a7806343bceed3dcd8602e
gretmn102 <erlgreywatson@gmail.com>
2023-05-09 16:09:01 +0300

    solve the 3 task

---
+++ Task3/src/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static string PrintAndReadLine(string description, string defaultValue)
+    {
+        System.Console.Write(description);
+        string? answer = System.Console.ReadLine();
+        if (String.IsNullOrEmpty(answer))
+        {
+            return defaultValue;
+        }
+        return answer;
+    }
+
+    private static void Main(string[] args)
+    {
+        string name = PrintAndReadLine("Введите свое имя: ", "Анонимус");
+        string birthYear = PrintAndReadLine("Введите свой год рождения: ", "???");
+        string zodiac = PrintAndReadLine("Введите ваш знак зодиака: ", "???");
+        string work = PrintAndReadLine("Вы работаете в/на: ", "???");
+
+        Console.WriteLine($"Вас зовут {name}. Вам {birthYear} лет. Ваш знак зодиака — {zodiac}. Вы работает в/на {work}.");
+    }
+}
