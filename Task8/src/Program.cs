d39037314dc8cb633836279f33bba6657b8e943b
gretmn102 <erlgreywatson@gmail.com>
2023-05-09 19:34:08 +0300

    solve the 8 task

---
+++ Task8/src/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите сообщение: ");
+        string message = Console.ReadLine();
+
+        Console.Write("Введите кол-во повторений: ");
+        int count = Int32.Parse(Console.ReadLine());
+
+        for (int i = 0; i < count; i++)
+        {
+            Console.WriteLine($"{message} x{i + 1}");
+        }
+    }
+}
