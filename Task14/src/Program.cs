ca8ce5590228b63d824b91fa1a604cef5155fc9f
gretmn102 <erlgreywatson@gmail.com>
2023-05-15 19:45:35 +0300

    solve 14 task

---
+++ Task14/src/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите имя: ");
+        string name = Console.ReadLine() ?? "";
+
+        Console.Write("Введите символ для рамки: ");
+        char frameChar = (char)Console.Read();
+
+        for (int i = 0; i < 2 + name.Length + 2; i++)
+        {
+            Console.Write(frameChar);
+        }
+        Console.WriteLine();
+
+        Console.WriteLine($"{frameChar} {name} {frameChar}");
+
+        for (int i = 0; i < 2 + name.Length + 2; i++)
+        {
+            Console.Write(frameChar);
+        }
+        Console.WriteLine();
+    }
+}
