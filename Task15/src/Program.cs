154581b70c516f952c8a6495d39d3f9135d92a73
gretmn102 <erlgreywatson@gmail.com>
2023-05-17 23:42:25 +0300

    solve 15 task

---
+++ Task15/src/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int maxAttemptsCount = 3;
+
+        string veryStrongPassword = "12345";
+        string secretMessage = "Не думай о розовом слоне.";
+
+        int attemptsCount = 0;
+        while (true)
+        {
+            Console.Write("Введите пароль, чтобы увидеть тайное послание: ");
+            string typedPassword = Console.ReadLine() ?? "";
+            if (typedPassword != veryStrongPassword)
+            {
+                attemptsCount++;
+                if (attemptsCount >= maxAttemptsCount)
+                {
+                    Console.WriteLine("Попыток больше не осталось. Вы так и не смогли прочесть тайное послание.");
+                    break;
+                }
+
+                Console.WriteLine($"Пароль неверный. Осталось {maxAttemptsCount - attemptsCount} попыток.");
+                continue;
+            }
+
+            Console.WriteLine("Пароль верный. Секретное сообщение:");
+            ConsoleColor currentColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(secretMessage);
+            Console.ForegroundColor = currentColor;
+            break;
+        }
+    }
+}
