4e46e45246eea8f821a905a2e17103391f3a0c02
gretmn102 <erlgreywatson@gmail.com>
2023-05-23 09:17:57 +0300

    solve 18 task

---
+++ Task18/src/Program.cs
@@ -0,0 +1,54 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.Write("Type the expression: ");
+            string input = Console.ReadLine() ?? "";
+            int currentDeep = 0;
+            int maxDeep = 0;
+            bool isIncorrect = false;
+            for (int i = 0; i < input.Length; i++)
+            {
+                char c = input[i];
+                switch (c)
+                {
+                    case '(':
+                        currentDeep++;
+                        if (maxDeep < currentDeep)
+                        {
+                            maxDeep = currentDeep;
+                        }
+                        break;
+
+                    case ')':
+                        if (currentDeep <= 0)
+                        {
+                            Console.WriteLine($"Extra opening parenthesis at {i}.");
+                            isIncorrect = true;
+                            continue;
+                        }
+                        currentDeep--;
+                        break;
+
+                    default:
+                        Console.WriteLine($"Unexpected '{c}' character at {i}.");
+                        break;
+                }
+            }
+            if (currentDeep > 0)
+            {
+                isIncorrect = true;
+                Console.WriteLine($"Requires {currentDeep} closing parentheses");
+            }
+
+            if (isIncorrect)
+            {
+                Console.WriteLine("The expression is incorrect.");
+            }
+
+            Console.WriteLine($"Maximum nesting depth is {maxDeep}");
+        }
+    }
+}
