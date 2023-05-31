29bf7e213cad3991a9c2676fa8d038e2cdf49401
gretmn102 <erlgreywatson@gmail.com>
2023-06-01 00:15:20 +0300

    solve 26 task

---
+++ Task26/src/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main()
+    {
+        string input = "Дана строка с текстом, используя метод строки `String.Split()` получить массив слов, которые разделены пробелом в тексте и вывести массив, каждое слово с новой строки.";
+        string[] words = input.Split(" ");
+        for (int i = 0; i < words.Length; i++)
+        {
+            Console.WriteLine(words[i]);
+        }
+    }
+}
