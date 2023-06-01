f236cd03688985acdf5384bee7a8169ed3bac4a4
gretmn102 <erlgreywatson@gmail.com>
2023-06-01 08:22:03 +0300

    solve 27 task

---
+++ Task27/src/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main()
+    {
+        int arrayLength = 10;
+        int[] array = new int[arrayLength];
+        for (int i = 0; i < arrayLength; i++)
+        {
+            array[i] = i;
+        }
+
+        // деление по модулю нужно в случае, если пользователь введет число больше длины исходного массива
+        int shift = -3 % arrayLength;
+
+        int[] resultArray = new int[arrayLength];
+        int diff = arrayLength - shift;
+        for (int i = 0; i < arrayLength; i++)
+        {
+            resultArray[i] = array[(diff + i) % arrayLength];
+        }
+
+        Console.WriteLine("Исходный массив:");
+        for (int i = 0; i < arrayLength; i++)
+        {
+            Console.Write($"{array[i]} ");
+        }
+        Console.WriteLine();
+
+        Console.WriteLine($"Итоговый массив, сдвинутый на {shift}:");
+        for (int i = 0; i < arrayLength; i++)
+        {
+            Console.Write($"{resultArray[i]} ");
+        }
+        Console.WriteLine();
+    }
+}
