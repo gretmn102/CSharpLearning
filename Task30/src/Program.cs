7bedf49b6b4aa1960313b9b67d462063ce50bcfd
gretmn102 <erlgreywatson@gmail.com>
2023-06-03 04:50:27 +0300

    solve 30 task

---
+++ Task30/src/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main()
+    {
+        while (true)
+        {
+            Console.Write("Введите целочисленное число: ");
+            string userInput = Console.ReadLine() ?? "";
+            if (int.TryParse(userInput, out int number))
+            {
+                Console.WriteLine($"Вы ввели {number}");
+                break;
+            }
+            Console.WriteLine($"`{userInput}` — не число!");
+        }
+    }
+}
