7e2a1607d128e89cb7aa603b97627f9ec594b4ba
gretmn102 <erlgreywatson@gmail.com>
2023-06-03 04:38:41 +0300

    solve 29 task

---
+++ Task29/README.md
@@ -0,0 +1,19 @@
+﻿# Задача №29: UI progress bar element
+Разработайте функцию, которая рисует некий бар (Healthbar, Manabar) в определённой позиции. Она также принимает некий закрашенный процент.
+
+При 40% бар выглядит так:
+```
+[####______]
+```
+
+## Build
+
+Build:
+```
+dotnet build
+```
+
+Just run:
+```
+dotnet run --project src\App.csproj
+```
---
+++ Task29/src/Program.cs
@@ -0,0 +1,41 @@
+﻿internal class Program
+{
+    const char startChar = '[';
+    const char fullChar = '#';
+    const char emptyChar = '_';
+    const char endChar = ']';
+
+    static void ProgressBarDisplay(string description, int value, int maxValue, int stepsCount = 10)
+    {
+        if (stepsCount <= 0)
+        {
+            throw new ArgumentException($"must be greater than or equal to zero", nameof(stepsCount));
+        }
+
+        if (!(0 <= value && value <= maxValue))
+        {
+            throw new ArgumentException($"must be greater than or equal to zero and less than or equal {maxValue}", nameof(value));
+        }
+
+        Console.Write(startChar);
+        int stepLength = maxValue / stepsCount;
+        int to = value / stepLength;
+        for (int i = 0; i < to; i++)
+        {
+            Console.Write(fullChar);
+        }
+        for (int i = to; i < stepsCount; i++)
+        {
+            Console.Write(emptyChar);
+        }
+        Console.Write(endChar);
+        Console.WriteLine($" {value}/{maxValue} {description}");
+    }
+
+    private static void Main()
+    {
+        ProgressBarDisplay("HP", 55, 250, 16);
+        ProgressBarDisplay("MP", 55, 100, 13);
+        ProgressBarDisplay("SP", 250, 1000);
+    }
+}
