b09d344a25cd3b568980e131acb3be55a8fc0634
gretmn102 <erlgreywatson@gmail.com>
2023-06-04 10:40:03 +0300

    solve 32 task

---
+++ Task32/README.md
@@ -0,0 +1,14 @@
+﻿# Задача №31: Канзас Сити Шафл
+Реализуйте функцию Shuffle, которая перемешивает элементы массива в случайном порядке.
+
+## Build
+
+Build:
+```
+dotnet build
+```
+
+Just run:
+```
+dotnet run --project src\App.csproj
+```
---
+++ Task32/src/Program.cs
@@ -0,0 +1,55 @@
+﻿internal class Program
+{
+    public static void ArrayRemoveAt<T>(ref T[] arr, int index)
+    {
+        if (!(0 <= index && index < arr.Length)) {
+            throw new ArgumentException("index must be greater than or equal to zero and less than the length of the array", nameof(index));
+        }
+        int length = arr.Length - 1;
+
+        for (int i = index; i < length; i++)
+        {
+            arr[i] = arr[i + 1];
+        }
+        Array.Resize(ref arr, length);
+    }
+
+    private static void ArrayShuffle<T>(ref T[] srcArray)
+    {
+        Random r = new();
+        int srcArrayLength = srcArray.Length;
+
+        int length = srcArrayLength;
+        T[] shuffledArray = new T[length];
+
+        int randomSrcArrayIndex;
+        for (int i = 0; i < length; i++)
+        {
+            randomSrcArrayIndex = r.Next(0, srcArrayLength);
+            shuffledArray[i] = srcArray[randomSrcArrayIndex];
+            ArrayRemoveAt(ref srcArray, randomSrcArrayIndex);
+            srcArrayLength--;
+        }
+        srcArray = shuffledArray;
+    }
+
+    private static void ArrayPrint<T>(ref T[] array)
+    {
+        int length = array.Length;
+        for (int i = 0; i < length; i++)
+        {
+            Console.Write($"{array[i]} ");
+        }
+        Console.WriteLine();
+    }
+
+    private static void Main()
+    {
+        int[] array = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        Console.WriteLine("Исходный массив:");
+        ArrayPrint(ref array);
+        ArrayShuffle(ref array);
+        Console.WriteLine("Перемешанный массив:");
+        ArrayPrint(ref array);
+    }
+}
