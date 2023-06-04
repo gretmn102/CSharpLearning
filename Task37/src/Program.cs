d3d7272d0c2803d000d41f090b17afeb9be2ddf3
gretmn102 <erlgreywatson@gmail.com>
2023-06-04 15:04:30 +0300

    solve 37 task

---
+++ Task37/src/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    public static void IEnumerablePrint<T>(IEnumerable<T> result)
+    {
+        foreach (var item in result)
+        {
+            Console.Write($"{item} ");
+        }
+        Console.WriteLine();
+    }
+
+    private static void Main()
+    {
+        // Лень писать что-то полноценное, потому воспользуюсь обычными множествами
+        Console.Write("Первая коллекция: ");
+        HashSet<int> firstSet = new() { 1, 2, 1 };
+        IEnumerablePrint(firstSet);
+        Console.Write("Вторая коллекция: ");
+        HashSet<int> secondSet = new() { 3, 2 };
+        IEnumerablePrint(secondSet);
+
+        var resultSet = firstSet.Union(secondSet);
+        Console.Write("Объеденная первая коллекция со второй: ");
+        IEnumerablePrint(resultSet);
+    }
+}
