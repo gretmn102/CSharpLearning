ec5423934c93268ab18d6161d2ccae49857ba3f8
gretmn102 <erlgreywatson@gmail.com>
2023-06-04 10:02:20 +0300

    task31: add the ability to collect coins

--- Task31/src/Program.cs
+++ Task31/src/Program.cs
@@ -1,5 +1,26 @@
 ﻿internal class Program
 {
+    public static void ArrayRemoveAt<T>(ref T[] arr, int index)
+    {
+        if (!(0 <= index && index < arr.Length)) {
+            throw new ArgumentException("index must be greater than or equal to zero and less than the length of the array", nameof(index));
+        }
+        int length = arr.Length - 1;
+
+        for (int i = index; i < length; i++)
+        {
+            arr[i] = arr[i + 1];
+        }
+        Array.Resize(ref arr, length);
+    }
+
+    public static void ArrayAdd<T>(T item, ref T[] arr)
+    {
+        int length = arr.Length;
+        Array.Resize(ref arr, length + 1);
+        arr[length] = item;
+    }
+
     // я забыл, что значит структура:
 
     /// <summary>ID, он же выполняет роль спрайта</summary>
@@ -62,20 +83,113 @@
         }
     }
 
+    private static void HeroDraw()
+    {
+        CanvasSet(_heroId, _heroX, _heroY);
+    }
+
     const char _wallId = 'x';
 
-    const char _chestId = 'c';
+    const char _coinId = 'c';
 
     const char _emptySpaceId = ' ';
 
-    static char[,] _world = null!;
+    static char[,] _canvas = null!;
 
-    static void WorldInit(ref char[,] level)
+    static void CanvasInit(int width, int height)
+    {
+        _canvas = new char[height, width];
+    }
+
+    static void CanvasSet(char sprite, int x, int y)
+    {
+        _canvas[y, x] = sprite;
+    }
+
+    static void CanvasDraw()
+    {
+        int height = CanvasGetHeigth();
+        int width = CanvasGetWidth();
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                Console.Write(_canvas[y, x]);
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static int CanvasGetWidth()
+    {
+        return _canvas.GetLength(1);
+    }
+
+    static int CanvasGetHeigth()
+    {
+        return _canvas.GetLength(0);
+    }
+
+
+    static bool[,] _walls = null!;
+    static void WallInit(int width, int height)
+    {
+        _walls = new bool[height, width];
+    }
+
+    static void WallsSet(bool isWall, int x, int y)
+    {
+        _walls[y, x] = isWall;
+    }
+
+    static bool WallsGet(int x, int y)
+    {
+        return _walls[y, x];
+    }
+
+    static int WallsGetWidth()
+    {
+        return _walls.GetLength(1);
+    }
+
+    static int WallsGetHeigth()
+    {
+        return _walls.GetLength(0);
+    }
+
+
+    /// <summary>
+    /// Поскольку структуры пока не доступны, поэтому придется
+    /// завести массив из массивов двух int'ов, первый элемент
+    /// из которых является положением монеты на оси X, а
+    /// второй — положением на оси Y.
+    /// </summary>
+    static int[][] _coins = null!;
+
+    static void CoinsInit(int length)
+    {
+        _coins = new int[length][];
+    }
+
+    static void CoinsAddCoin(int coinX, int coinY)
+    {
+        var item = new int[] { coinX, coinY };
+        ArrayAdd(item, ref _coins);
+    }
+
+    static void CoinsRemoveCoin(int index)
+    {
+        ArrayRemoveAt(ref _coins, index);
+    }
+
+    static void Init(ref char[,] level)
     {
         int height = level.GetLength(0);
         int width = level.GetLength(1);
 
-        _world = level;
+        WallInit(width, height);
+        CoinsInit(0);
+        CanvasInit(width, height);
 
         char current;
         for (int y = 0; y < height; y++)
@@ -86,7 +200,11 @@
                 switch (current)
                 {
                     case _wallId:
-                    case _chestId:
+                        WallsSet(true, x, y);
+                        break;
+                    case _coinId:
+                        CoinsAddCoin(x, y);
+                        break;
                     case _emptySpaceId:
                         break;
                     case _heroId:
@@ -98,30 +216,39 @@
                 }
             }
         }
-
     }
 
-    static void WorldSetEntity(char entity, int x, int y)
+    static void WallsDraw()
     {
-        _world[y, x] = entity;
-    }
-
-    static char WorldGetEntity(int x, int y)
-    {
-        return _world[y, x];
-    }
-
-    static void WorldPrint()
-    {
-        int worldWidth = _world.GetLength(1);
-        int worldHeight = _world.GetLength(0);
-        for (int y = 0; y < worldHeight; y++)
+        int wallWidth = WallsGetWidth();
+        int wallHeight = WallsGetHeigth();
+        for (int y = 0; y < wallHeight; y++)
         {
-            for (int x = 0; x < worldWidth; x++)
+            for (int x = 0; x < wallWidth; x++)
             {
-                Console.Write(_world[y, x]);
+                if (WallsGet(x, y))
+                {
+                    CanvasSet(_wallId, x, y);
+                }
+                else
+                {
+                    CanvasSet(_emptySpaceId, x, y);
+                }
             }
-            Console.WriteLine();
+        }
+    }
+
+    private static int CoinsGetCount()
+    {
+        return _coins.Length;
+    }
+
+    private static void CoinsDraw()
+    {
+        for (int i = 0; i < _coins.Length; i++)
+        {
+            int[] coin = _coins[i];
+            CanvasSet(_coinId, coin[0], coin[1]);
         }
     }
 
@@ -163,30 +290,25 @@
 
         if (_heroX != heroNewX || _heroY != heroNewY)
         {
-            WorldSetEntity(_emptySpaceId, _heroX, _heroY);
             _heroX = heroNewX;
             _heroY = heroNewY;
-            WorldSetEntity(_heroId, _heroX, _heroY);
         }
     }
 
     private static void HeroCollisionWall(ref int heroNewX, ref int heroNewY)
     {
-        switch (WorldGetEntity(heroNewX, heroNewY))
+        bool isWall = WallsGet(heroNewX, heroNewY);
+        if (isWall)
         {
-            case _emptySpaceId:
-                break;
-            default:
-                heroNewX = _heroX;
-                heroNewY = _heroY;
-                break;
+            heroNewX = _heroX;
+            heroNewY = _heroY;
         }
     }
 
     private static void HeroCollisionWorldUpdate(ref int heroNewX, ref int heroNewY)
     {
-        int worldWidth = _world.GetLength(1);
-        int worldHeight = _world.GetLength(0);
+        int worldWidth = _walls.GetLength(1);
+        int worldHeight = _walls.GetLength(0);
 
         if (heroNewX < 0)
         {
@@ -207,22 +329,41 @@
         }
     }
 
+    static void HeroCoinCollision()
+    {
+        for (int i = 0; i < CoinsGetCount(); i++)
+        {
+            int[] coin = _coins[i];
+            if (_heroX == coin[0] && _heroY == coin[1])
+            {
+                CoinsRemoveCoin(i);
+                i--;
+            }
+        }
+    }
+
     static void PhysicsUpdate()
     {
         HeroCollisionUpdate();
+        HeroCoinCollision();
         HeroSlowDown();
     }
 
-    static void InfoPrint()
+    static void InfoDraw()
     {
         Console.WriteLine("WASD (← ↑ → ↓) — перемещение");
+        int coinsCount = CoinsGetCount();
+        Console.WriteLine($"Осталось собрать монет {coinsCount}");
     }
 
     static void GraphicsUpdate()
     {
         Console.Clear();
-        WorldPrint();
-        InfoPrint();
+        WallsDraw();
+        CoinsDraw();
+        HeroDraw();
+        CanvasDraw();
+        InfoDraw();
     }
 
     private static void Main()
@@ -234,8 +375,10 @@
             { 'c', ' ', ' ', 'x', 'c' }};
 
         Console.WriteLine("Loading level...");
-        WorldInit(ref level);
+        Init(ref level);
         Console.WriteLine("Level is loaded.");
+
+        Console.CursorVisible = false;
 
         GraphicsUpdate();
 
