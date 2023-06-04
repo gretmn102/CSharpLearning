e227e494c80b545309f5b2b8a915e705679ca767
gretmn102 <erlgreywatson@gmail.com>
2023-06-04 15:12:36 +0300

    solve 38 task

---
+++ Task38/src/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace App
+{
+    class User
+    {
+        string UserName { get; }
+        int Rating { get; }
+
+        public User(string userName, int rating)
+        {
+            UserName = userName;
+            Rating = rating;
+        }
+
+        public override string ToString()
+        {
+            return $"Имя пользователя: {UserName}\nЕго рейтинг: {Rating}";
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            User user = new("Михаил", 999);
+            Console.WriteLine(user);
+        }
+    }
+}
