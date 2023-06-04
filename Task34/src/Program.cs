ce6299318f86b8093bb0151770d6bcd0e6f5cf19
gretmn102 <erlgreywatson@gmail.com>
2023-06-04 11:36:34 +0300

    solve 34 task

---
+++ Task34/src/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main()
+    {
+        int shopBalance = 0;
+        int[] purchaseSums = new int[] { 10, 20, 5, 3 };
+        int length = purchaseSums.Length;
+        int currentPurchaseSum;
+        for (int i = 0; i < length; i++)
+        {
+            Console.Clear();
+            currentPurchaseSum = purchaseSums[i];
+            Console.WriteLine($"Баланс на счету магазина {shopBalance}");
+            Console.WriteLine($"На кассу подходит клиент с суммой покупки на {currentPurchaseSum} у.е.");
+            Console.WriteLine("(Нажмите любую клавишу, чтобы обслужить клиента)");
+            Console.ReadKey();
+            shopBalance += currentPurchaseSum;
+        }
+        Console.Clear();
+        Console.WriteLine($"Все клиенты обслужены, очередь пуста, баланс на счету магазина составляет {shopBalance}. Хорошая работа.");
+    }
+}
